3b72f6ce22b26b52cbbb131f493328fb48864f99
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-03-08 19:09:57 +0530

    #

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,50 +1,67 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import CartItem from "../components/CartItem";
-import { useEffect, useState } from "react";
+
+
+
 
 const Cart = () => {
-  const { cart } = useSelector((state) => state);
 
+  const {cart} = useSelector((state) => state);
+  console.log("Printing Cart");
+  console.log(cart);
   const [totalAmount, setTotalAmount] = useState(0);
 
-  useEffect(() => {
-    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
-  }, [cart]);
+  useEffect( () => {
+    setTotalAmount( cart.reduce( (acc, curr) => acc + curr.price,0) );
+  }, [cart])
 
   return (
     <div>
-      {cart.length > 0 ? (
-        <div>
-          <div>
-            {cart.map((item, index) => {
-              return <CartItem key={item.id} item={item} itemIndex={index} />;
-            })}
-          </div>
+  {
+    cart.length > 0  ? 
+    (<div>
 
-          <div>
-            <div>
-              <div>Your Cart</div>
-              <div>Summary</div>
-              <p>
-                <span>Total Items: {cart.length}</span>
-              </p>
-            </div>
 
-            <div>
-              <p>Total Amount : ${totalAmount}</p>
-              <button>Checkout Now</button>
-            </div>
-          </div>
-        </div>
-      ) : (
+      <div>
+        {
+          cart.map( (item,index) => {
+            return <CartItem key={item.id} item={item} itemIndex={index} />
+          } )
+        }
+      </div>
+
+      <div>
+
         <div>
-          <h1>Cart Empty</h1>
-          <Link to={"/"}>
-            <button>Shop Now</button>
-          </Link>
+          <div>Your Cart</div>
+          <div>Summary</div>
+          <p>
+            <span>Total Items: {cart.length}</span>
+          </p>
         </div>
-      )}
+
+        <div>
+          <p>Total Amount: ${totalAmount}</p>
+          <button>
+            CheckOut Now
+          </button>
+        </div>
+
+      </div>
+
+
+    </div>) : 
+    (<div>
+      <h1>Cart Empty</h1>
+      <Link to={"/"}>
+        <button>
+          Shop Now
+        </button>
+      </Link>
+    </div>)
+  }
     </div>
   );
 };
