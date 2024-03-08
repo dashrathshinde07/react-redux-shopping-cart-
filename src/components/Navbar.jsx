18c9e8340c72430612ad83109e01366953e285c6
dashrathshinde07 <dashrathbshinde@gmail.com>
2024-03-08 13:45:28 +0530

    #

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,30 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="flex flex-row justify-between">
+        <NavLink to="/">
+          <div>
+            <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/store-logo-design%2C-online-shopping-store-logo-template-b96b23a1a56e685fe30519cbf20883bc_screen.jpg?ts=1662903800" />
+          </div>
+        </NavLink>
+
+        <div>
+          <NavLink to="/">
+            <p>Home</p>
+          </NavLink>
+
+          <NavLink>
+            <div>
+              <FaShoppingCart />
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
+import Product from "../components/Product";
+
 const Home = () => {
   const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoding] = useState(false);
+  const [posts, setPosts] = useState([]);
 
-  return <div>Home</div>;
+  async function fethProductData() {
+    setLoding(true);
+
+    try {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+
+      setPosts(data);
+    } catch (error) {
+      console.log("Error aagya jii");
+      setPosts([]);
+    }
+    setLoding(false);
+  }
+
+  useEffect(() => {
+    fethProductData();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : posts.length > 0 ? (
+        <div>
+          {posts.map((post) => (
+            <Product key={posts.id} post={post} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No Data Found</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
